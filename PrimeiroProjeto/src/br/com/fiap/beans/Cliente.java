cf62e760677fe5399fc84350e05fef3d9eb3880b
Matheus Freitas <140122752+MatheusHenriqueNF@users.noreply.github.com>
2024-08-26 12:06:43 -0300

    Add files via upload

---
+++ PrimeiroProjeto/src/br/com/fiap/beans/Cliente.java
@@ -0,0 +1,22 @@
+package br.com.fiap.beans;
+
+public class Cliente {
+
+    // visibilidade, tipo de dado e atributo
+    private String nome;
+    private int idade;
+    private double peso;
+
+    //metdos Setters (Entrada) Getters (Saíde)
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+
+
+
+}
---
+++ PrimeiroProjeto/src/br/com/fiap/main/TesteOlaMundo.java
@@ -0,0 +1,13 @@
+package br.com.fiap.main;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TesteOlaMundo {
+
+    //Método de execução
+    public static void main(String[] args) {
+
+        System.out.println("Ola mundo!!!!!!!!");
+
+    }
+}
